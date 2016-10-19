43c1cd0b3a7b864ffeb3616997d8fb7b72c48ca7
Ganesha <reekoheek@gmail.com>
2016-10-19 21:17:52 +0700

    INTRODUCED scrolling mechanism to ion-content

--- ion-content.js
+++ ion-content.js
@@ -3,7 +3,44 @@ import xin from 'xin';
 import './css/ion-content.css';
 
 class IonContent extends xin.Component {
+  get props () {
+    return {
+      scrollX: {
+        type: Boolean,
+        value: false,
+      },
 
+      scrollY: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
+
+  ready () {
+    if (this.scrollX) {
+      this.classList.add('scroll-x');
+    } else {
+      this.classList.remove('scroll-x');
+    }
+
+    if (this.scrollY) {
+      this.classList.add('scroll-y');
+    } else {
+      this.classList.remove('scroll-y');
+    }
+  }
+
+  attached () {
+    if (this.scrollX || this.scrollY) {
+      this.__templateModel.addEventListener('focusing', () => {
+        this.async(() => {
+          this.scrollTop = 0;
+          this.scrollLeft = 0;
+        });
+      });
+    }
+  }
 }
 
 xin.define('ion-content', IonContent);
