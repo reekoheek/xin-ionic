7f78847408fbbf0e9aeb4c9aa010f50d5db9a35b
Ganesha <reekoheek@gmail.com>
2016-11-17 19:14:34 +0700

    INTRODUCED ion-slides

---
+++ ion-slides.js
@@ -0,0 +1,30 @@
+import xin from 'xin';
+import Swiper from 'swiper';
+
+class IonSlides extends xin.Component {
+  get template () {
+    return `
+    <div class="swiper-container swiper-container-horizontal">
+      <div class="swiper-wrapper">
+        <slot></slot>
+      </div>
+    </div>
+    `;
+  }
+
+  ready () {
+    super.ready();
+
+    [].forEach.call(this.querySelectorAll('ion-slide'), el => el.classList.add('swiper-slide'));
+  }
+
+  attached () {
+    this.async(() => {
+      this.swiper = new Swiper(this.$$('.swiper-container'));
+    }, 1);
+  }
+}
+
+xin.define('ion-slides', IonSlides);
+
+export default IonSlides;
