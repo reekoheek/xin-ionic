4ef3ba3af54939c939d78ad40e086d097ee56e93
Ganesha <reekoheek@gmail.com>
2017-01-20 15:04:27 +0700

    fixed slide not build when created from xin-repeat

--- ion-slides.js
+++ ion-slides.js
@@ -19,23 +19,25 @@ class IonSlides extends xin.Component {
     `);
   }
 
-  ready () {
-    super.ready();
-
-    [].forEach.call(this.querySelectorAll('ion-slide'), el => el.classList.add('swiper-slide'));
-  }
-
   attached () {
     super.attached();
 
-    this.async(() => {
-      System.import('swiper').then(Swiper => {
-        this.swiper = new Swiper(this.$$('.swiper-container'), this.options);
-      });
-    }, 1);
+    System.import('swiper').then(Swiper => {
+      this.swiper = new Swiper(this.$$('.swiper-container'), this.options);
+    });
   }
 }
 
 xin.define('ion-slides', IonSlides);
+
+class IonSlide extends xin.Component {
+  created () {
+    super.created();
+
+    this.classList.add('swiper-slide');
+  }
+}
+
+xin.define('ion-slide', IonSlide);
 
 export default IonSlides;
